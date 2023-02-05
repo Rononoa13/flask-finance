e9bb006badbf574c46d4cde20ff8432a5163eb96
Rononoa13 <sumitluitel42@gmail.com>
2023-02-05 22:49:29 +0545

    Connected database and log in if user is registered

--- database.py
+++ database.py
@@ -6,8 +6,14 @@ connection.row_factory = sqlite3.Row
 
 # 
 INSERT_USER = "INSERT INTO users (username, hash) VALUES (?, ?)"
-
+SELECT_USERS = "SELECT * FROM users WHERE username = ?"
 
 def add_user(name, hash):
     with connection:
         connection.execute(INSERT_USER, (name, hash))
+
+def get_user(username):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SELECT_USERS, [username])
+        return cursor.fetchall()
