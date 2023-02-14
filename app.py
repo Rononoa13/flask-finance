7c5a433242d45c0f98444d42d6eb5a1f737a9f48
Rononoa13 <sumitluitel42@gmail.com>
2023-02-14 22:24:55 +0545

    Added history html file and logic in app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
-
+import requests
 # from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session, jsonify
+from flask import Flask, flash, redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 from tempfile import mkdtemp
@@ -12,7 +12,6 @@ from helpers import apology, login_required, lookup, usd
 
 import database
 
-import json
 """
 Apparently directly using jsonify from Flask did not as it could not 
 serialize the Stream object in `user_cash_from_db`. When doing jsonify(user_cash_from_db)
@@ -59,9 +58,31 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    return apology("TODO")
+    userid = session["user_id"]
+    username = database.get_one_user(userid)
+    
+    # symbol = session["symbol"]
+    # # print(symbol_list)
+    # from_api = lookup(symbol)
+    
+    
+    # symbol, share and price
+    transaction_db = database.get_sy_sh_pr(userid)
+    
+    # price_from_api =  
+    # Get default cash form user in the database.
+    cash_db = database.get_cash(userid)
+    print(f"type(cash_db) - {type(cash_db)}")
+    cash = cash_db["cash"]
+    print(f"type(cash) {type(cash)}")
 
+    # current datetime
+    now = datetime.now()
+    date_time = now.strftime("%d-%B-%Y")
+    return render_template("index.html", username=username, date_time=date_time, database=transaction_db, cash=cash)
+    # return apology("TODO")
 
+symbol_list = []
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
 def buy():
@@ -70,7 +91,9 @@ def buy():
         return render_template("buy.html")
     else:
         symbol = request.form.get("symbol")
-
+        session["symbol"] = symbol
+        # symbol_list.append(session["symbol"])
+        # print(symbol_list)
         # Render an apology if the input is blank or the symbol does not exist 
         if not symbol:
             return apology("Must provide symbol")
@@ -112,7 +135,13 @@ def buy():
 @login_required
 def history():
     """Show history of transactions"""
-    return apology("TODO")
+    user_id = session["user_id"]
+    # currentPrice = float(requests.get(
+    #                 f"https://cloud.iexapis.com/stable/stock/{ticker}/quote?token={API_KEY}").json()["latestPrice"])
+    
+    transaction_db = database.get_all_transaction_db(user_id)
+
+    return render_template("history.html", transaction_db=transaction_db)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -135,7 +164,7 @@ def login():
 
         # Query database for username
         rows = database.get_user(request.form.get("username"))
-        print(f"Rows - {rows}")
+        # print(f"Rows - {rows}")
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
@@ -143,7 +172,8 @@ def login():
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
-
+        # session["username"] = request.form.get("username")
+        # print(session["username"])
         # Redirect user to home page
         return redirect("/")
 
@@ -213,7 +243,7 @@ def register():
         try:
             # INSERT INTO table_name (column1, column2 ...) VALUES (value1, value2, ...)
             new_user = database.add_user(username, hash)
-            print(f"New user - {new_user}")
+            # print(f"New user - {new_user}")
         except KeyError:
             return apology("USERNAME ALREADY EXIST")
 
@@ -229,4 +259,45 @@ def register():
 @login_required
 def sell():
     """Sell shares of stock"""
-    return apology("TODO")
+    if request.method == "GET":
+        user_id = session["user_id"]
+        user_symbol = database.get_only_symbol(user_id)
+        return render_template("sell.html", symbols=user_symbol)
+    # return apology("TODO")
+    else:
+        symbol = request.form.get("symbol")
+        shares = int(request.form.get('shares'))
+        print(f"symbol - {str(symbol)}")
+        
+        if not symbol:
+            return apology("Must provide symbol")
+        
+        stock = lookup(symbol)
+        if not stock:
+            return apology("Symbol does not exist.")
+
+        shares = int(request.form.get("shares"))
+        if shares < 0:
+            return apology("Input positive number")
+        # the user does not own that many shares of the stock.
+
+        transaction_value = shares * stock["price"]
+
+        user_id = session["user_id"]
+      
+        user_cash_from_db = database.get_cash(user_id)
+
+        # Get value from cash column from users table in database
+        user_cash = user_cash_from_db['cash']
+
+        update_cash = user_cash + transaction_value
+
+        database.update_cash(update_cash, user_id)
+
+        current_date = datetime.now()
+
+        # INSERT data into transaction table
+        database.add_entry_in_transaction(user_id, stock["symbol"], shares, stock["price"])
+        # Redirect user to home page
+        flash("Sold!")
+        return redirect("/")
